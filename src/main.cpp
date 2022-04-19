86e17b633779c4d086b11b9bc390b753d3125ee4
ba58smith <ba58smith@gmail.com>
2022-04-19 10:43:16 -0400

    Formatting and comment changes - no actual code changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <ESP32Encoder.h>
-#include "esp_task_wdt.h"
 #include <heltec.h>
 #include <ESP_FlexyStepper.h>
 
@@ -8,20 +7,18 @@
 #define BPM_MULTIPLIER 30  // SWAG at this point
 #define VOLUME_TO_MM_CONVERSION 25 // BAS: 25.0 is an approximation - need to get it exact
 
-/**
- * @brief Updates only the area of the OLED that displays the Volume
- */
-
 int topcornerX = 1;
 int topcornerY = 30;
 int bottomcornerX = 128;
 int bottomcornerY = 64;
 
-void update_oled(float volume, uint8_t bpm)
-{
-  Heltec.display->setColor(BLACK); //color to use
+/**
+ * @brief Updates only the area of the OLED that displays the Volume
+ */
+void update_oled(float volume, uint8_t bpm) {
+  Heltec.display->setColor(BLACK);
   Heltec.display->fillRect(topcornerX, topcornerY, bottomcornerX, bottomcornerY);
-  Heltec.display->setColor(WHITE); //color to use
+  Heltec.display->setColor(WHITE);
   String rpm_vol_str = String(bpm) + "       " + String(volume, 1);
   Heltec.display->drawString(10, 26, rpm_vol_str);
   Heltec.display->display();
@@ -45,31 +42,32 @@ const uint8_t motor_step_pin = 27;
 const uint8_t motor_direction_pin = 14;
 const uint8_t limit_switch_pin = 39;
 
-static IRAM_ATTR void bpm_encoder_cb(void *arg)
-{
+// define all the ISR's and ISR callbacks
+static IRAM_ATTR void bpm_encoder_cb(void *arg) {
   digitalWrite(LED_BUILTIN, !led);
   bpm_knob_interrupt_fired = true;
 }
 
-static IRAM_ATTR void volume_encoder_cb(void *arg)
-{
+static IRAM_ATTR void volume_encoder_cb(void *arg) {
   digitalWrite(LED_BUILTIN, !led);
   volume_knob_interrupt_fired = true;
 }
 
-void limit_switch_isr()
-{
+// BAS: check this interrupt inside the two while() loops, to execute an emergency stop?
+void limit_switch_isr() {
   limit_switch_interrupt_fired = true;
 }
-
-//extern bool loopTaskWDTEnabled;
-//extern TaskHandle_t loopTaskHandle;
 
 ESP32Encoder bpm_encoder(true, bpm_encoder_cb);
 ESP32Encoder volume_encoder(true, volume_encoder_cb);
 
-float get_volume_as_float()
-{
+/**
+ * @brief Read the current value of the Volume knob, convert it to a float,
+ * and return 1/10 of the value, so the display can be to 1 decimal. Always
+ * use this function to read the knob, so that all volume values will be
+ * consistent! 
+ */
+float get_volume_as_float() {
   float count = (float)volume_encoder.getCount() / 10;
   if (count > 6.0)
   {
@@ -84,8 +82,12 @@ float get_volume_as_float()
   return count;
 }
 
-uint8_t get_bpm_as_int()
-{
+/**
+ * @brief Read the current value of the BPM knob.. Always
+ * use this function to read the knob, so that all BPM values will be
+ * consistent! 
+ */
+uint8_t get_bpm_as_int() {
   uint8_t count = bpm_encoder.getCount();
   if (count > 6)
   {
@@ -103,53 +105,40 @@ uint8_t get_bpm_as_int()
 ESP_FlexyStepper stepper;
 
 float motorStepsPerMillimeter = 25; // BAS: verified
-//float motorStepsPerRevolution = 200;
-float speedInMillimetersPerSecond = 200; // controlled by bpm knob
 float distanceToMoveInMillimeters = 76;  // controlled by volume knob
-float accelerationInMillimetersPerSecondPerSecond = 100;
-float decelerationInMillimetersPerSecondPerSecond = 100;
-uint8_t motor_direction = 1;
+float speedInMillimetersPerSecond = 200; // controlled by bpm knob
+float accelerationInMillimetersPerSecondPerSecond = 100; // controlled by bpm knob
+float decelerationInMillimetersPerSecondPerSecond = 100; // controlled by bpm knob
 
-float target_position_in_mm = VOLUME_TO_MM_CONVERSION;
-uint8_t bpm_desired = 1 * BPM_MULTIPLIER; // for testing
+float target_position_in_mm = VOLUME_TO_MM_CONVERSION; // sets the initial target, until the volume knob is turned
+uint8_t bpm_desired = 1 * BPM_MULTIPLIER; // sets the initial BPM, until the BPM knob is turned
 
-void setup()
-{
-
-  //loopTaskWDTEnabled = true;
+void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(limit_switch_pin, INPUT_PULLUP);
   Serial.begin(115200);
 
-  // Enable the weak pull down resistors
-  //ESP32Encoder::useInternalWeakPullResistors=DOWN;
   // Enable the weak pull up resistors
   ESP32Encoder::useInternalWeakPullResistors = UP;
 
-  // use pin 17 and 16 for the bpm encoder
   bpm_encoder.attachSingleEdge(bpm_DT_pin, bpm_CLK_pin);
+  // following line, along with a 0.1uF cap to GND, is for debouncing
   bpm_encoder.setFilter(1023);
   bpm_encoder.setCount(1);
 
-  // use pin 19 and 18 for the volume encoder
   volume_encoder.attachSingleEdge(volume_DT_pin, volume_CLK_pin);
-  // BAS: following added, along with a 0.1uF cap to GND, for debouncing
   volume_encoder.setFilter(1023);
   volume_encoder.setCount(30);
 
   //attachInterrupt(limit_switch_pin, LOW);
 
-  //esp_task_wdt_add(loopTaskHandle);
-
   stepper.connectToPins(motor_step_pin, motor_direction_pin);
-  stepper.startAsService(); // BAS: this s/b 0, not 1
   stepper.setStepsPerMillimeter(motorStepsPerMillimeter);
-  //stepper.setStepsPerRevolution(motorStepsPerRevolution);
   stepper.setAccelerationInMillimetersPerSecondPerSecond(accelerationInMillimetersPerSecondPerSecond);
   stepper.setDecelerationInMillimetersPerSecondPerSecond(decelerationInMillimetersPerSecondPerSecond);
   stepper.setSpeedInMillimetersPerSecond(speedInMillimetersPerSecond);
 
-  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
+  Heltec.begin(true, false, true);
   Heltec.display->clear();
   Heltec.display->display();
   Heltec.display->resetOrientation();
@@ -183,8 +172,6 @@ void loop() {
   if (volume_knob_interrupt_fired) {
 
     update_oled(get_volume_as_float(), get_bpm_as_int());
-    // BAS: put code here to update the OLED with the new value, and the appropriate stepper
-    // variable (volume, rate).
     target_position_in_mm = get_volume_as_float() * VOLUME_TO_MM_CONVERSION;
     stepper.setTargetPositionInMillimeters(target_position_in_mm);
     volume_knob_interrupt_fired = false;
@@ -192,11 +179,10 @@ void loop() {
 
   if (bpm_knob_interrupt_fired) {
     update_oled(get_volume_as_float(), get_bpm_as_int());
-    // BAS: put code here to update the OLED with the new value, and the appropriate stepper
-    // variable (volume, rate).
     bpm_desired = get_bpm_as_int() * BPM_MULTIPLIER;
     stepper.setSpeedInMillimetersPerSecond(bpm_desired);
     stepper.setAccelerationInMillimetersPerSecondPerSecond(bpm_desired * 1.2);
+    stepper.setDecelerationInMillimetersPerSecondPerSecond(bpm_desired * 1.2);
     bpm_knob_interrupt_fired = false;
   }
 
@@ -208,7 +194,7 @@ void loop() {
     stepper.processMovement();
   }
 
-  // Now reverse it for the exhale stroke
+  // now reverse it for the exhale stroke
   target_position_in_mm *= -1.0;
   stepper.setTargetPositionInMillimeters(HOME);
   while (!stepper.motionComplete()) {
@@ -220,4 +206,4 @@ void loop() {
   target_position_in_mm *= -1.0;
   stepper.setTargetPositionInMillimeters(target_position_in_mm);
   
-}
+} // end loop()
