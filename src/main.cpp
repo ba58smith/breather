7924c5beae722600bf22962ab7954ab431d43312
ba58smith <ba58smith@gmail.com>
2022-04-21 11:55:41 -0400

    First stab at auto correction of BPM

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
 #include <elapsedMillis.h>
 
 #define HOME 0
-#define BPM_MULTIPLIER 30  // SWAG at this point
+//#define BPM_MULTIPLIER 30  // BAS: no longer needed, I don't think
 #define VOLUME_TO_MM_CONVERSION 33.3 // BAS: 33.3 is according to Forrest's spec document
+#define INITIAL_BPM 25
+#define INITIAL_VOLUME 30
 
 int topcornerX = 1;
 int topcornerY = 30;
@@ -16,6 +18,9 @@ int bottomcornerY = 64;
 /**
  * @brief Updates only the area of the OLED that displays the Volume
  */
+
+// BAS: modify to display desired_bpm and measured_bpm on the left side of the display.
+// At the end of setup(), measured_bpm s/b "??", cause it won't be measured until the first while() is complete
 void update_oled(float volume, uint8_t bpm) {
   Heltec.display->setColor(BLACK);
   Heltec.display->fillRect(topcornerX, topcornerY, bottomcornerX, bottomcornerY);
@@ -112,8 +117,10 @@ float speedInMillimetersPerSecond = 200; // just a starting value - controlled b
 float accelerationInMillimetersPerSecondPerSecond = 100; // just a starting value - controlled by bpm knob
 float decelerationInMillimetersPerSecondPerSecond = 100; // just a starting value - controlled by bpm knob
 
-float target_position_in_mm = VOLUME_TO_MM_CONVERSION; // sets the initial target, until the volume knob is turned
-uint8_t bpm_desired = 1 * BPM_MULTIPLIER; // sets the initial BPM, until the BPM knob is turned
+float target_position_in_mm = 3.0 * VOLUME_TO_MM_CONVERSION; // sets the initial target, until the volume knob is turned (3.0 liters)
+uint8_t desired_bpm = INITIAL_BPM; // sets the initial BPM, until the BPM knob is turned
+
+float ERROR_CORRECTION_THRESHOLD = 0.005;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -126,11 +133,11 @@ void setup() {
   bpm_knob.attachSingleEdge(bpm_DT_pin, bpm_CLK_pin);
   // following line, along with a 0.1uF cap to GND, is for debouncing
   bpm_knob.setFilter(1023);
-  bpm_knob.setCount(25); // set the initially-displayed BPM
+  bpm_knob.setCount(INITIAL_BPM); // set the initially-displayed BPM
 
   volume_knob.attachSingleEdge(volume_DT_pin, volume_CLK_pin);
   volume_knob.setFilter(1023);
-  volume_knob.setCount(30); // set the initially-displayed volume (30 is displayed as 3.0)
+  volume_knob.setCount(INITIAL_VOLUME); // set the initially-displayed volume (30 is displayed as 3.0)
 
   //attachInterrupt(limit_switch_pin, LOW);
 
@@ -171,6 +178,11 @@ void setup() {
 } // setup;
 
 void loop() {  
+  float desired_piston_speed = 0.0;
+  float calculated_bpm = 0.0;
+  float calculated_error_percentage = 0.0;
+  float error_correction_factor = 0.0;
+
   if (volume_knob_interrupt_fired) {
 
     update_oled(get_volume_as_float(), get_bpm_as_int());
@@ -181,7 +193,8 @@ void loop() {
 
   if (bpm_knob_interrupt_fired) {
     update_oled(get_volume_as_float(), get_bpm_as_int());
-    bpm_desired = get_bpm_as_int(); float desired_piston_speed = abs(target_position_in_mm) * 2.0 * bpm_desired  / 60;
+    desired_bpm = get_bpm_as_int();
+    desired_piston_speed = abs(target_position_in_mm) * 2.0 * desired_bpm  / 60; // Forrest's formula
     stepper.setSpeedInMillimetersPerSecond(desired_piston_speed);
     stepper.setAccelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
     stepper.setDecelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
@@ -190,35 +203,43 @@ void loop() {
 
   delay(200);
   
-  // begine the inhale stroke  
+  // begin the inhale stroke  
   while (!stepper.motionComplete()) {
     // Note: Any code added to this loop must execute in no more than 0.05 milliseconds.
     bpm_timer = 0;
     stepper.processMovement();
   }
-  delay(1500); // for testing when not connected to the stepper
+  delay(1500); // for testing calculated_bpm when not connected to the stepper
   // save the value of bpm_timer immediately on exiting the while()
   uint32_t while_loop_time = bpm_timer;
   // calculate BPM from bpm_timer: ms_in_a_half_minute / while_loop_ms
-  float calculated_bpm = 30000.0 / while_loop_time;
-  Serial.println("inhale while() bpm_timer and calculated BPM: " + String(while_loop_time) + " / " + String(calculated_bpm, 1));
+  calculated_bpm = 30000.0 / while_loop_time;
+  Serial.println("inhale stroke while_timer and calculated BPM: " + String(while_loop_time) + " / " + String(calculated_bpm, 1));
+
+  // calculate automatic error correction after inhale stroke
+  calculated_error_percentage = calculated_bpm / desired_bpm;
+  if (calculated_error_percentage < 1.0 - ERROR_CORRECTION_THRESHOLD || calculated_error_percentage > 1.0 + ERROR_CORRECTION_THRESHOLD) {
+    error_correction_factor = 2.0 - calculated_error_percentage; // 1.03 becomes 0.97, and 0.97 becomes 1.03
+  } // if error is less than ERROR_CORRECTION_THRESHOLD, error_correction_factor remains at 0.0
  
   // now reverse it for the exhale stroke
   // check the interrupts between the strokes for better updating of the OLED
   if (volume_knob_interrupt_fired) {
-
     update_oled(get_volume_as_float(), get_bpm_as_int());
     target_position_in_mm = get_volume_as_float() * VOLUME_TO_MM_CONVERSION;
     // do NOT setTargetPosition at this point - on the exhale stroke, targetPosition is always HOME (eventually TDC)
+    error_correction_factor = 0.00; // set back to 0.00 any time one of the knobs changes values
     volume_knob_interrupt_fired = false;
   }
 
   if (bpm_knob_interrupt_fired) {
     update_oled(get_volume_as_float(), get_bpm_as_int());
-    bpm_desired = get_bpm_as_int(); float desired_piston_speed = abs(target_position_in_mm) * 2.0 * bpm_desired  / 60;
+    desired_bpm = get_bpm_as_int();
+    desired_piston_speed = abs(target_position_in_mm) * 2.0 * desired_bpm  / 60; // Forrest's formula
     stepper.setSpeedInMillimetersPerSecond(desired_piston_speed);
     stepper.setAccelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
     stepper.setDecelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
+    error_correction_factor = 0.00; // set back to 0.00 any time one of the knobs changes values
     bpm_knob_interrupt_fired = false;
   }
 
@@ -229,19 +250,39 @@ void loop() {
   // inside if (volume_knob_interrupt_fired) {}, so when the next line executes, it would become positive.
   target_position_in_mm *= -1.0;
   stepper.setTargetPositionInMillimeters(HOME);
+  if (error_correction_factor != 0.0) { // the error is big enough that we want to make a correction
+    desired_piston_speed *= error_correction_factor;
+    stepper.setSpeedInMillimetersPerSecond(desired_piston_speed);
+    stepper.setAccelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
+    stepper.setDecelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
+    error_correction_factor = 0.0;
+  }
+  
   while (!stepper.motionComplete()) {
     // Note: Any code added to this loop must execute in no more than 0.05 milliseconds.
     bpm_timer = 0;
     stepper.processMovement();
   }
-  delay(1234); // for testing when not connected to the stepper
+  delay(1234); // for testing calculated_bpm when not connected to the stepper
   while_loop_time = bpm_timer;
   calculated_bpm = 60000.0 / (while_loop_time * 2.0); // BAS: does ths round the way I want?
-  Serial.println("exhale while_loop_time and calculated BPM: " + String(while_loop_time) + " / " + String(calculated_bpm, 1));
+  Serial.println("exhale stroke while_loop_time and calculated BPM: " + String(while_loop_time) + " / " + String(calculated_bpm, 1));
+
+  // calculate automatic error correction after exhale stroke
+  calculated_error_percentage = calculated_bpm / desired_bpm;
+  if (calculated_error_percentage < 1.0 - ERROR_CORRECTION_THRESHOLD || calculated_error_percentage > 1.0 + ERROR_CORRECTION_THRESHOLD) {
+    error_correction_factor = 2.0 - calculated_error_percentage; // 1.03 becomes 0.97, and 0.97 becomes 1.03
+  } // if error is less than ERROR_CORRECTION_THRESHOLD, error_correction_factor remains at 0.0
 
   //delay(100);
-  // change directions for the next inhale
+  // change directions for the next inhale stroke
   target_position_in_mm *= -1.0;
   stepper.setTargetPositionInMillimeters(target_position_in_mm);
-  
+  if (error_correction_factor != 0.0) { // the error is big enough that we want to make a correction
+    desired_piston_speed *= error_correction_factor;
+    stepper.setSpeedInMillimetersPerSecond(desired_piston_speed);
+    stepper.setAccelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
+    stepper.setDecelerationInMillimetersPerSecondPerSecond(desired_piston_speed * desired_piston_speed);
+    error_correction_factor = 0.0;
+  }
 } // end loop()
